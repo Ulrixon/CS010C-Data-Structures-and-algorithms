db589059399ea94b622436721cb914e161c029d0
Ulrixon <ryan70235@gmail.com>
2023-10-08 13:36:25 -0700

    implement member functions

---
+++ 2.27Lab2_double_linklist/IntList.cpp
@@ -0,0 +1,45 @@
+#include "IntList.h"
+using namespace std;
+
+IntList::IntList()
+{
+    IntNode *dummyNodeHead = new IntNode(0);
+    IntNode *dummyNodeTail = new IntNode(0);
+    this->dummyHead = dummyNodeHead;
+    this->dummyTail = dummyNodeTail;
+    dummyHead->prev = nullptr;
+    dummyNodeHead->next = dummyNodeTail;
+    dummyNodeTail->prev = dummyNodeHead;
+    dummyNodeTail->next = nullptr;
+}
+
+void IntList::push_front(int value)
+{
+    IntNode *currNode = new IntNode(value);
+    currNode->next = this->dummyHead->next;
+    currNode->prev = this->dummyHead;
+    this->dummyHead->next->prev = currNode;
+    this->dummyHead->next = currNode;
+}
+
+void IntList::pop_front()
+{
+    if (!this->dummyHead->next->data == 0)
+    {
+        this->dummyHead->next = this->dummyHead->next->next;
+        this->dummyHead->next->prev = this->dummyHead;
+    }
+}
+
+void IntList::push_back(int value)
+{
+    IntNode *currNode = new IntNode(value);
+    currNode->next = this->dummyTail;
+    currNode->prev = this->dummyTail->prev;
+    this->dummyTail->prev->next = currNode;
+    this->dummyTail->prev = currNode;
+}
+
+void IntList::pop_back()
+{
+}
